2758ada3117816d43ddabbaefb5ddb6cdb4a818b
Cyber-Lord <Umaralfuruq02@gmail.com>
2022-02-22 16:41:19 +0100

    Added static files

--- Rashid/urls.py
+++ Rashid/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('api-auth/', include('rest_framework.urls'))
+    # path('authjwt/', include('djoser.urls.jwt')),
+    # path('api-auth/', include('rest_framework.urls'))
 ]
+urlpatterns += staticfiles_urlpatterns(),
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
