6a82f843328310848ed8d177f6c3a9ec619b8a01
abdoulrasheed <abdoulrasheedibrahim47@gmail.com>
2022-02-26 01:02:07 +0100

    Reset everything

---
+++ store/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import User, Category, Item, Station, Order, Remittance, Truck
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'is_superuser', 'last_login', 'first_name', 'last_name', 'other_name',
+                    'phone', 'is_sales_person', 'is_store_keeper', 'is_station_manager']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title']
+
+class TruckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Truck
+        fields = ['driver', 'number']
+
+class ItemSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    class Meta:
+        model = Item
+        fields = ['name', 'category']
+
+class RemittanceSerializer(serializers.ModelSerializer):
+    truck = TruckSerializer()
+    class Meta:
+        model = Remittance
+        fields = ['station', 'status', 'rejection_note', 'truck', 'amount',
+                    'teller', 'remittance_id', 'timestamp',]
+
+class StationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Station
+        fields = ['name', 'address', 'station_manager']
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['truck', 'item', 'order_date', 'order_status']
+
---
+++ store/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from django.urls.conf import include
+from rest_framework_nested import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register('items', views.ItemViewSet, basename='items')
+router.register('categories', views.CategoryViewSet)
+router.register('stations', views.StationViewSet)
+router.register('users', views.UserViewSet)
+router.register('truck', views.TruckViewSet)
+router.register('remittance', views.RemittanceViewSet)
+router.register('orders', views.OrderViewSet)
+
+urlpatterns = router.urls 
