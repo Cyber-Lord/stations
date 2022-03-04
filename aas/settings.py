d58c3959c886b220f4ecb054f46fa6f2c342dc8d
Abdulrasheed Ibrahim <abdulrasheedibrahim47@gmail.com>
2022-03-04 11:26:53 +0100

    Remove duplicate app label

--- aas/settings.py
+++ aas/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework_nested',
     'rest_framework',
-    'django.contrib.staticfiles',
     'drf_yasg',
     'djoser',
     'main',
